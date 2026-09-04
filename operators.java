77fe995c9f8f4f49a3e7691b3ccb7adff4105cb3
Niha <nihashaik01@gmail.com>
2026-09-04 16:47:32 +0530

    operators

--- operators.java
+++ operators.java
@@ -1,19 +1,19 @@
-class operator{
-    public static void main(String args[]){
-        int a=20;
-        int b=30;
-        int c=a;
-        a+=b;
+class operators{
+public static void main(String args[]){
+int a=20;
+int b=30;
+int c=a;
+ a+=b;
 System.out.println("a value is: "+a);
 a-=b;
 System.out.println("a value is: "+a);
 a*=b;
 System.out.println("a value is: "+a);
 a/=b;
-System.out.println("a value is: "+a);
+System.out.print("a value is: "+a);
 a%=b;
-System.out.println("a value is: "+a);
-
-
-    }
+System.out.print("a value is: "+a);
 }
+}
+
+
