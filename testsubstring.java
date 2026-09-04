961fc835af85ffbbaf9e9c78c6dfe90d55f76ffd
Niha <nihashaik01@gmail.com>
2026-09-04 16:07:24 +0530

    test sub string

---
+++ testsubstring.java
@@ -0,0 +1,8 @@
+class testsubstring{
+public static void main(String args[]){
+String s1="Niha Reddy";
+System.out.println(s1);
+System.out.println(s1.substring(4));
+System.out.println(s1.substring(0,4));
+}
+}
