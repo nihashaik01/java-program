6838438d25f1f616f8bb677a1f6fe8f00435c585
Niha <nihashaik01@gmail.com>
2026-09-04 16:05:39 +0530

    Start End

---
+++ startend.java
@@ -0,0 +1,7 @@
+class startend{
+public static void main(String[] args);
+String str="StringCompare";
+System.out.println(str.startWith("String"));
+System.out.println(str.endsWith("Compare"));
+}
+}
