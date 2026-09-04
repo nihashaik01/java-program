52d5869b19d9fd20ea4ab202fbbc9f7c2c8394ac
Niha <nihashaik01@gmail.com>
2026-09-04 16:56:06 +0530

    Variable

---
+++ Variable.java
@@ -0,0 +1,11 @@
+class Variable{
+static int c=30;
+int a=10;
+public static void main(String[] args){
+int b=20;
+System.out.println(c);
+System.out.println(b);
+Variable obj = new Variable(); //Object Creation
+System.out.println(obj.a);
+}
+}
