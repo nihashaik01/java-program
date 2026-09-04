d99a12c887e0c6070350f2f431d79bac5f334e57
Niha <nihashaik01@gmail.com>
2026-09-04 16:50:51 +0530

    Relationl

---
+++ relationl.java
@@ -0,0 +1,12 @@
+class relationl{
+public static void main(String args[]){
+int a=40;
+int b=26;
+System.out.println(a==b);
+System.out.println(a!=b);
+System.out.println(a>=b);
+System.out.println(a<=b);
+System.out.println(a>b);
+System.out.println(a<b);
+}
+}
