b9cccba3997fb2706aaea5cc44a383b2ab7bc9ca
Niha <nihashaik01@gmail.com>
2026-09-04 16:48:39 +0530

    Postive Negative

--- PostiveNegative.java
+++ PostiveNegative.java
@@ -1,6 +1,6 @@
 public class PostiveNegative{
 public static void main (String []args){
-int number =2;
+int number =-5;
 String result= (number>+0)? "positive" : "negative";
 System.out.println(result);
 }
