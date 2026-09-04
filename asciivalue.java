a3d8601679e38359d8f7c2213f3dffa94a875e8a
Niha <nihashaik01@gmail.com>
2026-09-04 16:18:48 +0530

    ascii value

--- asciivalue.java
+++ asciivalue.java
@@ -1,8 +1,15 @@
-class ASCIIvalueexample{
+class asciivalue{
 public static void main(String[]args){
-char[] charArray = {'H','e','l','l','o','','W','o','r','l','d';
-
-String result=new String(charArray,1,6);
-System.out.println(result);
+char ch='a';
+System.out.println(ch);
+System.out.println(0 + ch);
+System.out.println(ch - 0);
+System.out.println(1* ch);
+System.out.println(0 - ch);
+System.out.println(1+ch);
+int n=98;
+char x=(char) n;
+System.out.println(n);
+System.out.println(x);
 }
 }
