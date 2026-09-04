bc1f42ebc18ec23d685277ea547875dcfabbd223
Niha <nihashaik01@gmail.com>
2026-09-04 16:01:00 +0530

    check passfail

---
+++ checkpassfail.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class checkpassfail{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+
+int marks = sc.nextInt();
+
+System.out.println("The mark is" + marks);
+
+if (marks>=50){
+ System.out.println("PASS");
+}
+else{
+System.out.println("FAIL");
+}
+System.out.println("DONE");
+
+}
+}
