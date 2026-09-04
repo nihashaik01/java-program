569dcf69003a373ae4f4273ea2f24902467edb44
Niha <nihashaik01@gmail.com>
2026-09-04 16:26:46 +0530

    Employee details

---
+++ Employeedetails.java
@@ -0,0 +1,17 @@
+class employeedetails{
+static String company name= "ABC technologies";
+
+String employeeid= "E1025";
+String employeename= "Priya sharma";
+ 
+public static void main(String[]args);
+ String department= "software development";
+System.out.println("===THE EMPOLYEE DETAIL===");
+System.out.println("THE COMPANY NAME : "+companyname);
+
+company obj=new company();
+System.out.println("THE EMPLOYEEID : "obj.employeeid);
+System.out.println("THE EMPLOYEENAME : "obj.employeename);
+System.out.println("THE DEPARTMENT:" +department);
+}
+}
