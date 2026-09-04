e565cecb3ddf1e61f4553a58d1dab59c334d64bd
Niha <nihashaik01@gmail.com>
2026-09-04 16:20:20 +0530

    book summary

---
+++ booksummary.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class booksummary{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+
+do{
+int totalcustomer=0;
+int totalticket=0;
+char continuebooking;
+
+do{
+System.out.println("***CUSTOMER NAME: ") ;
+String name=sc.nextLine();
+
+System.out.println("TICKET : ") ;
+int ticket=sc.nextInt();
+
+System.out.println("CUSTOMER : ") ;
+int customer=sc.nextInt();
+}
+Totaltickets=("totaltickets + tickets");
+System.out.println("CONTINUE(Y/N) : ") ;
+continuebooking=sc.nextchart();
+}while(continuebooking==y);
+
+System.out.println("***BOOKING SUMMARY*** : ");
+System.out.println("TOTAL CUSTOMERS :"+ totalcustomers" );
+System.out.println("TOTAL TICKETS :"+ totaltickets");
+}
+}
+}
+
