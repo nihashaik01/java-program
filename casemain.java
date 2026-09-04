009a2e0976fbc6f017c450c3deed56d06afff3e1
Niha <nihashaik01@gmail.com>
2026-09-04 16:00:52 +0530

    case main

---
+++ casemain.java
@@ -0,0 +1,12 @@
+class casemain{
+public static void main(String[]args){
+String s="Computer";
+System.out.println(s.toUpperCase());
+System.out.println(s.toLowerCase());
+System.out.println(s.startsWith("c"));
+System.out.println(s.endsWith("r"));
+System.out.println(s.charAt(2));
+System.out.println(s.charAt(1));
+System.out.println(s.length());
+}
+}
