e582578f454b14a67ea8eba6b49ab54ecf18ed2f
Niha <nihashaik01@gmail.com>
2026-09-04 16:25:16 +0530

    else

---
+++ else.java
@@ -0,0 +1,16 @@
+class gradedispaly{
+public static void main(String[] args)
+{
+
+int grade=80;
+if (grade>70)
+   System.out.println("grade a");
+else if (grade>60)
+   System.out.println("grade b");
+else if (grade>50)
+   System.out.println("grade c");
+else
+   System.out.println("Fail");
+
+}
+}
