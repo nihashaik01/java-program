3f0a2bbfeb25e89828213bad832c773fec1d7374
Niha <nihashaik01@gmail.com>
2026-09-04 16:24:39 +0530

    compare String example

---
+++ compareStringexample.java
@@ -0,0 +1,8 @@
+public class compareStringexample{
+public static void main(String args[]){
+String str1="Apple";
+String str2="Banana";
+System.out.println("Equal?"+ str1.equals(str2));
+System.out.println("CompareTo result:"+ str1.compareTo(str 2));
+}
+}
