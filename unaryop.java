d90544aae1cbaa6591c711f01444a216a19c7cb2
Niha <nihashaik01@gmail.com>
2026-09-04 16:56:14 +0530

    Unaryop

---
+++ unaryop.java
@@ -0,0 +1,11 @@
+class unaryop{
+public static void main(String[] args){
+int a=10;
+System.out.println("Before Postincement the values of a: "+a++);
+System.out.println(After Postincement the values of a:"+a);
+
+a=10;
+System.out.println("Before Preincement the values of a: "+++a);
+System.out.println(After Preincement the value of a: "+a);
+}
+}
