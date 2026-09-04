404f3fc5509a3b45432041aff292f7f8ff0283fa
Niha <nihashaik01@gmail.com>
2026-09-04 16:30:56 +0530

    jagged

---
+++ jagged.java
@@ -0,0 +1,15 @@
+class jagged{
+public static void main(String args[]){
+int[][]jagged=new int[3][];
+jagged[0]=new int[]{1,2};
+jagged[1]=new int[]{3,4,5};
+jagged[2]=new int[]{6};
+System.out.println("jagged Array Output:");
+for(int i=0; i<jagged.length;i++){
+for(int j=0; j<jagged[i].length; j++){
+System.out.print(jagged[i][j]+"    ");
+}
+System.out.println();
+}
+}
+}
