51395a3e3bcde2e24bb5a79fa00a4d3ec881e497
Niha <nihashaik01@gmail.com>
2026-09-04 16:07:00 +0530

    termary operator

---
+++ ternaryoperator.java
@@ -0,0 +1,8 @@
+public class ternaryoperator{
+public static void main (String args[]){
+int a=6;
+String result= (a%2 ==0)? "EVEN" : "ODD";
+System.out.println("the number "+a+" result is " + result);
+}
+}
+
