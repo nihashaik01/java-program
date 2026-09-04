1ba37f25e3d76efbee98bd663c33e6f7454e282b
Niha <nihashaik01@gmail.com>
2026-09-04 16:50:01 +0530

    product Details

--- productDetails.java
+++ productDetails.java
@@ -1,38 +1,38 @@
 import java.util.Scanner;
-public class productDetails{
-String proudctId;
+public class productDetails {
+String productId;
 String product;
-Double price;
-productDetails(){
+double price;
+productDetails() {
 productId = "P100";
 product = "Laptop";
-price += 50000;
-public void productDetails(String id, String name, double p) {
+price = 50000;
 }
-proudctId = id;
+public productDetails(String id, String name, double p) {
+productId = id;
 product = name;
 price = p;
 }
-void display(){
-System.out.println("Product Id : " + Product Id);
-System.out.println("Product : " + Product);
-System.out.println("Price : " + Price);
+void display() {
+System.out.println("Product Id : " + productId);
+System.out.println("Product : " + product);
+System.out.println("Price : " + price);
 }
-public static void main(String args[]){
-Scanner sc=new Scanner(System.in);
+public static void main(String args[]) {
+Scanner sc = new Scanner(System.in);
 System.out.println("***PRODUCT DETAILS***");
 System.out.println("\nUsing Default constructor");
-productDetails p1= new productDetails();
+productDetails p1 = new productDetails();
 p1.display();
 System.out.println("\nUsing Parameterized constructor");
 System.out.print("Enter product id: ");
-String id=sc.nextLine();
+String id = sc.nextLine();
 System.out.print("enter product name: ");
-String name= sc.nextLine();
-System.out.print("enter print: ");
-String name= sc.nextDouble();
-productDetails p2= new productDetails(id, name,price);
+String name = sc.nextLine();
+System.out.print("enter price: ");
+double price = sc.nextDouble();
+productDetails p2 = new productDetails(id, name, price);
 p2.display();
+sc.close();
 }
 }
-
