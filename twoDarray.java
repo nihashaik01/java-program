312b83968f0c63b696cff73c608ae239efb514f3
Niha <nihashaik01@gmail.com>
2026-09-04 16:56:21 +0530

    Two Darray

---
+++ twoDarray.java
@@ -0,0 +1,19 @@
+class twoDarray{
+public static void main(String args[]){
+int rows=4, columns=4;
+int[][] array= new int[rows][columns];
+int value=1;
+for(int i=0; i< rows; i++){
+for(int j=0; j<columns; j++){
+array[i][j]=value; value++;
+}
+}
+System.out.println("The 2D array is: ");
+for(int i=0; i< rows; i++){
+for(int j=0; j< columns; j++){
+System.out.print(array[i][j]+ " ");
+}
+System.out.println();
+}
+}
+}
