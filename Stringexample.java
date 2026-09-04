5436722ce017e332a52d8b888950335156117c86
Niha <nihashaik01@gmail.com>
2026-09-04 15:53:25 +0530

    Stringexample

---
+++ Stringexample.java
@@ -0,0 +1,8 @@
+class Stringexample{
+public static void main(String args[]){
+String s1="Apple";
+String s2="Banana";
+System.out.println("Equal:"+ s1.equals(s2));
+System.out.println("CompareTo result:"+ s1.compareTo(s2));
+}
+}
