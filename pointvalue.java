fe7f7ade53c436c3ea02bd126f827334b90572e0
Niha <nihashaik01@gmail.com>
2026-09-04 16:48:45 +0530

    point value

---
+++ pointvalue.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class pointvalue{
+public static void main(String[]args){
+Scanner input=new Scanner(System.in);
+System.out.print("enter an float:");
+float myFloat=input.nextFloat();
+System.out.println("float entered"+myFloat);
+}
+}
