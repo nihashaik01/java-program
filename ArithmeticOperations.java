d59020b45470c96d5ef17ad6b3f187a0315b85db
Niha <nihashaik01@gmail.com>
2026-09-04 16:17:52 +0530

    Arithmetic Operations

---
+++ ArithmeticOperations.java
@@ -0,0 +1,17 @@
+class ArithmeticOperations{
+public static void main(String args){
+int a=55;
+int b=30;
+int sum;
+sum= a+b;
+int diff = a-b;
+int prod = a*b;
+int quo = a / b;
+int rem = a % b; 
+System.out.println("The Sum is : " =sum);
+System.out.println("The difference is : "+ diff);
+System.out.println("The product is : "+ prod);
+System.out.println("The quotient is : "+ quo);
+Ssystem.out,println("The Remainder is :  "+ rem);
+}
+}
