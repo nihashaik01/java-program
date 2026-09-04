0b9fcce94d4aadc54cc1726993d056866f844bdf
Niha <nihashaik01@gmail.com>
2026-09-04 15:58:28 +0530

    ASCII value Example

---
+++ ASCIIvalueexample.java
@@ -0,0 +1,8 @@
+class ASCIIvalueexample{
+public static void main(String[]args){
+char[] charArray = {'h','e','l','l','o',' ','W','o','r','l','d'};
+
+String result=new String(charArray,1,6);
+System.out.println(result);
+}
+}
