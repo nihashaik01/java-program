830fd3fe46a1fb8fa5012cd1a42e915e2a429293
Niha <nihashaik01@gmail.com>
2026-09-04 16:55:59 +0530

    Variables

--- variables.java
+++ variables.java
@@ -1,7 +1,7 @@
 class variables{
-public static void main (String[]args){
+public static void main(String[]args){
 Double number=-10.6;
-System.out.Println(5);
-System.out.Println(number);
+System.out.println(5);
+System.out.println(number);
 }
 }
