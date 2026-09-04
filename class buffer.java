70be84726f4f3cd5c0bfecf5aaf0c04e6d312e8d
Niha <nihashaik01@gmail.com>
2026-09-04 16:24:12 +0530

    class buffer

---
+++ class buffer.java
@@ -0,0 +1,10 @@
+class buffer{
+public static void main(String args[]){
+StringBuffer sb1=new StringBuffer[];
+Ssystem.out.println("sb1(default)):
+StringBuffer sb2=new StringBuffer("Hello");
+System.out.println("sb2(with string)):
+StringBuffer sb3=new StringBuffer[50];
+System.out.println("sb3(with capacity 50)):
+}
+}
