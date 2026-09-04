642fafe4f2ed69feddb9864fa41adb48721c4534
Niha <nihashaik01@gmail.com>
2026-09-04 15:46:43 +0530

    relatinal

---
+++ relatinal.java
@@ -0,0 +1,12 @@
+class fordemo {
+    public static void main(String args[]){
+        int n= 10;
+        for (int i=0; i<n; i++){
+            break;
+        
+            }
+            System.out.println("the value of i is: "+i);
+        
+        }
+    }
+}
