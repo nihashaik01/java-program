e2e909a23432356d3e58f14f72d797423f8a68cc
Niha <nihashaik01@gmail.com>
2026-09-04 16:06:37 +0530

    table 6

---
+++ table6.java
@@ -0,0 +1,11 @@
+class table5{
+public static void main(String args[]){
+for (int i=1; i<=10; i++){
+System.out.print(" 1 x " + i + " = " + (1*i));
+System.out.print(" 2 x " + i + " = " + (2*i));
+System.out.print(" 3 x " + i + " = " + (3*i));
+System.out.print(" 4 x " + i + " = " + (4*i));
+System.out.print(" 5 x " + i + " = " + (5*i));
+}
+}
+}
