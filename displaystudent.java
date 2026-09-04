8fcd7addd84af06a1dea919072e0cc77a2090670
Niha <nihashaik01@gmail.com>
2026-09-04 16:25:42 +0530

    display student

---
+++ displaystudent.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class displaystudent{
+static void displayStudent(String name,int age, double marks){
+System.out.println(" student name= "+name);
+System.out.println("age= "+age);
+System.out.println("marks = "+marks);
+}
+public static void main(String args[]){
+}
+}
