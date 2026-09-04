d30cd93f1263c9a0ab7d17dc610637ca71aa5cbc
Niha <nihashaik01@gmail.com>
2026-09-04 16:30:51 +0530

    java Features

---
+++ JavaFeatures.java
@@ -0,0 +1,13 @@
+public class JavaFeatures{
+public static void main(String[]args){
+System.out.println("***JAVA FEATURES***");
+System.out.println();
+System.out.println("1. Platform Independent");
+System.out.println("2. Object Oriented");
+System.out.println("3. Robust");
+System.out.println("4.Secure");
+System.out.println("5.Portable");
+System.out.println("6.Multithreaded");
+System.out.println("7.Distributed");
+}
+}
