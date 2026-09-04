5782191a6ac864cd589bb250e6fc8e1cda4644cd
Niha <nihashaik01@gmail.com>
2026-09-04 16:05:04 +0530

    Railway Ticket System

---
+++ RailwayTicketSystem.java
@@ -0,0 +1,9 @@
+class RailwayTicketSystem {
+public static void main(String[] args) {
+StringBuffer sb =new StringBuffer("passenegr: priya\n");
+System.out.println("***railwayticket***");
+sb.append("Coach:B1");
+sb.replace(23,25,"D2");
+System.out.println("\n updated ticket" +sb);
+}
+}
