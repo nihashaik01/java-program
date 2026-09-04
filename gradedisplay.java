a3cafc9873255bc0d7d080f03e76c51a07d7d87c
Niha <nihashaik01@gmail.com>
2026-09-04 16:28:59 +0530

    grade display

---
+++ gradedisplay.java
@@ -0,0 +1,13 @@
+class gradedisplay{
+public static void main(String[] args)
+{
+
+int grade=80;
+if (grade>70)
+   System.out.println("grade a");
+if (grade>60)
+   System.out.println("grade b");
+if (grade>50)
+   System.out.println("grade c");
+}
+}
