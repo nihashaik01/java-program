0d872fa679814f7ec9c3d9161f24f134c36afa0c
Niha <nihashaik01@gmail.com>
2026-09-04 16:04:53 +0530

    purchased items

---
+++ purchaseditems.java
@@ -0,0 +1,5 @@
+import java.util.Scanner;
+class purchaseditems{
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.println("Enter purchase item list");
