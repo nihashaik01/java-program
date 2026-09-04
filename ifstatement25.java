75d66e809a6b1285923282e59b3bdbe08c891cdb
Niha <nihashaik01@gmail.com>
2026-09-04 16:29:48 +0530

    if statement 25

---
+++ ifstatement25.java
@@ -0,0 +1,25 @@
+class ifstatement{
+public static void main(String args[]){
+int  a=25;
+int  b=10;
+if (a==b){
+System.out.println("a is equal to b");
+}
+if (a!=b){
+System.out.println("a is not equal to b");
+}
+if (a>=b){
+System.out.println("a is greater equal to b");
+}
+if (a<=b){
+System.out.println("a is less equal to b");
+}
+if (a<b){
+System.out.println("a is less and equal to b");
+}
+if (a>b){
+System.out.println("a is greater and equal to b");
+}
+
+}
+}
